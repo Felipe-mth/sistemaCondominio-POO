3ff80eb3f0c5f9a94451a80ae51fd746e9d70beb
gabrie-masson <gabrielferreiramasson@gmail.com>
2021-11-17 20:13:59 -0300

    criando o projeto maven

---
+++ sistema_condominio_poo/src/test/java/poo/sistema_condominio/AppTest.java
@@ -0,0 +1,20 @@
+package poo.sistema_condominio;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
