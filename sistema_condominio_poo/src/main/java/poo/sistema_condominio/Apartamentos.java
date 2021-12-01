be91467dd22aa2e105eccdcb3ea7732385062b5a
GabrielHenrique <gabrielhenriquedps@gmail.com>
2021-12-01 17:14:39 -0300

    Criando a class GerenciaPermissoes e alterando a class Apartamentos

--- sistema_condominio_poo/src/main/java/poo/sistema_condominio/Apartamentos.java
+++ sistema_condominio_poo/src/main/java/poo/sistema_condominio/Apartamentos.java
@@ -7,8 +7,55 @@ public class Apartamentos {
     private Estacionamento estacionamento;
     private Morador morador;
     
-    public Apartamentos(Morador morador) {
+    public Apartamentos(Morador morador, int nApartamento, int andar, int bloco, Estacionamento estacionamento) {
+        this.morador = morador;
+        this.nApartamento = nApartamento;
+        this.andar = andar;
+        this.bloco =  bloco;
+        this.estacionamento = estacionamento;
+    }
+
+    public Morador getMorador() {
+        return morador;
+    }
+
+    public void setMorador(Morador morador) {
         this.morador = morador;
     }
+
+    public Estacionamento getEstacionamento(){
+        return estacionamento;
+    }
+
+    public void setEstacionamento(Estacionamento estacionamento){
+        this.estacionamento = estacionamento;
+    }
+
+    public  int getnApartamento(){
+        return nApartamento;
+    }
+
+    public void setnApartamento(int nApartamento){
+        this.nApartamento = nApartamento;
+    }
+
+    public int getBloco(){
+        return bloco;
+    }
+
+    public void setBloco(int bloco){
+        this.bloco = bloco;
+    }
     
+    public int getAndar(){
+        return andar;
+    }
+
+    public void setAndar(int andar){
+        this.andar = andar;
+    }
+    
+    public void estacionar(Apartamentos morador, Apartamentos apartamento){
+
+    }
 }
---
+++ sistema_condominio_poo/src/main/java/poo/sistema_condominio/GerenciaPermissoes.java
@@ -0,0 +1,55 @@
+package poo.sistema_condominio;
+
+public class GerenciaPermissoes {
+
+    private int horarioVisita;
+    private int periodoVisita;
+    private int dataVisita;
+    private Pessoa pessoa;
+    private Apartamentos apartamento;
+
+
+
+    public int getHorarioVisita(){
+        return horarioVisita;
+    }
+
+    public void setHorarioVisita(int horarioVisita ){
+        this.horarioVisita = horarioVisita;
+    }
+
+    public int getPeriodoVisita(){
+        return periodoVisita;
+    }
+
+    public void setPeriodoVisita( int periodoVisita){
+        this.periodoVisita =  periodoVisita;
+    }
+
+    public int getDataVisita(){
+        return dataVisita;
+    }
+
+    public void setDataVisita(int dataVisita){
+        this.dataVisita = dataVisita;
+    }
+
+    public Pessoa getPessoa(){
+        return pessoa;
+    }
+
+    public void setPessoa( Pessoa pessoa){
+        this.pessoa = pessoa;
+    }
+
+    public Apartamentos getApartamento(){
+        return apartamento;
+    }
+
+    public void setApartamento( Apartamentos apartamento){
+        this.apartamento = apartamento;
+    }
+
+
+
+}
