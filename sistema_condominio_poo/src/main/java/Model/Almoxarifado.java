f96feceb0963ff2a402f87deea3cae0d4b64e3ad
Guilherme Silveira <guisilveira.cout@gmail.com>
2021-12-27 18:40:27 -0300

    Exclusão dos métodos de Almoxarifado e adicao de get e set de produtos

--- sistema_condominio_poo/src/main/java/Model/Almoxarifado.java
+++ sistema_condominio_poo/src/main/java/Model/Almoxarifado.java
@@ -21,9 +21,6 @@ public class Almoxarifado {
     private String email;
     private ArrayList<Produtos> produtos;
 
-    public Almoxarifado() {
-    }
-
     public Almoxarifado(int id, Funcionario funcionario, String telefone, String email) {
         this.Id = id;
         this.funcionario = funcionario;
@@ -62,52 +59,11 @@ public class Almoxarifado {
         this.telefone = telefone;
     }
 
-    public void verificaEstoque() {
-        System.out.println("----- LISTA DE PRODUTOS ----");
-        for (int i = 0; i < produtos.size(); i++) {
-            System.out.println("########################");
-            System.out.println("PRODUTO " + (i + 1));
-            System.out.println("ID: " + this.produtos.get(i).getId());
-            System.out.println("Nome: " + this.produtos.get(i).getNome());
-            System.out.println("Quantidade em estoque: " + this.produtos.get(i).getQuantidade());
-            System.out.println("Quantidade mínima: " + this.produtos.get(i).getQuantidadeMinima());
-            System.out.println("Fornecedor: " + this.produtos.get(i).getFornecedor());
-            System.out.println("Preço: " + this.produtos.get(i).getPreco());
-            System.out.println("########################");
-        }
+    public ArrayList<Produtos> getProdutos() {
+        return this.produtos;
     }
 
-    public void reabastecer(int valor, String produto) {
-        for(Produtos prod: produtos){
-            if(prod.getNome().equals(produto)){
-                prod.setQuantidade( prod.getQuantidade() + valor);
-                System.out.println("Produto reabastecido com sucesso");
-            }
-        }
-            
-             //private Scanner scan = new Scanner(System.in);
-
-        /*System.out.println("Selecione o produto que será reabastecido: ");
-        for (Produtos prod : produtos) {
-            System.out.println("Nome: " + prod.getNome());
-        }
-        String nomeProduto = scan.nextLine();
-
-        for (Produtos prod : produtos) {
-            if (prod.getNome() == nomeProduto) {
-                System.out.println("Digite a quantidade a ser abastecida: ");
-                int abastecimento = scan.nextInt();
-                prod.setQuantidade((prod.getQuantidade() + abastecimento));
-                System.out.println("Produto abastecido com sucesso!");
-            }
-        }
-
-        scan.close();*/
-    }
-
-    public void trocarFuncionario(Funcionario novoFuncionario) {
-        System.out.println("Trocando funcionário " + this.funcionario + "pelo funcionário " + novoFuncionario);
-        this.funcionario = novoFuncionario;
-        System.out.println("Troca realizada com sucesso!");
+    public void setProdutos(ArrayList<Produtos> produtos) {
+        this.produtos = produtos;
     }
 }
