870f92a01907e8d1f2014e87d4fbb6b120be708b
HewerP <73977874+HewerP@users.noreply.github.com>
2021-12-28 00:44:19 -0300

    Solução Bug JavaFX

--- sistema_condominio_poo/src/main/java/View/App.java
+++ sistema_condominio_poo/src/main/java/View/App.java
@@ -1,7 +1,9 @@
 package View;
 import java.io.IOException;
+import java.net.URL;
 //JavaFX Imports
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,13 +11,16 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    @FXML
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("resources/JavaFX/Main.fxml"));
+        URL fxmlLocation = getClass().getResource("/JavaFX/Main.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         Parent parent = fxmlLoader.load();
         Scene scene = new Scene(parent, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
