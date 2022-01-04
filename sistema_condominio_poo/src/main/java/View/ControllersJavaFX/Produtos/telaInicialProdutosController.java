b59448392748ed62c704b7a5cad8415f98ba0a84
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-01-04 17:16:28 -0300

    Adicionando Produtos e voltando pra tela inicial

--- sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Produtos/telaInicialProdutosController.java
+++ sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Produtos/telaInicialProdutosController.java
@@ -1,4 +1,45 @@
 package View.ControllersJavaFX.Produtos;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class telaInicialProdutosController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Button botaoAdicionarTelaProduto;
+
+    @FXML
+    private Button botaoTelaInicial;
+
+    @FXML
+    void adicionarProduto(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/JavaFX/Produtos/adicionarProduto.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    void voltarTelaInicial(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/JavaFX/telaInicial.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
