782c77546049a082e95903e19b38b11ac3cc82d2
Felipe Matheus Costa <ld17038@gmail.com>
2022-01-06 22:16:11 -0300

    choicebox conectada com banco

--- sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Espacos/adicionarEspacoController.java
+++ sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Espacos/adicionarEspacoController.java
@@ -1,20 +1,27 @@
 package View.ControllersJavaFX.Espacos;
 
+import DAO.DaoClasse.MoradorDAO;
+import Model.Morador;
+import static View.ControllersJavaFX.Espacos.adicionarCarroController.morador;
 import View.ControllersJavaFX.SceneController;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
 
-public class adicionarEspacoController {
-
+public class adicionarEspacoController implements Initializable{
+   static MoradorDAO morador = new MoradorDAO();
     @FXML
     private DatePicker Data;
 
     @FXML
-    private ChoiceBox<?> Morador;
+    private ChoiceBox<Integer> Morador;
 
     @FXML
     private Button botaoAdicionar;
@@ -34,5 +41,13 @@ public class adicionarEspacoController {
 
         controladorCena.voltarParaPaginaAnterior(event, caminho);
     }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ArrayList<Integer> reserva = new ArrayList();
+        for(Morador item: morador.findAll()){
+         reserva.add(item.getId());
+        }
+    }
+
 
 }
--- sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Pessoa/adicionarMoradorController.java
+++ sistema_condominio_poo/src/main/java/View/ControllersJavaFX/Pessoa/adicionarMoradorController.java
@@ -1,16 +1,23 @@
 package View.ControllersJavaFX.Pessoa;
 
+import DAO.DaoClasse.ApartamentosDAO;
+import Model.Apartamentos;
 import View.ControllersJavaFX.SceneController;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class adicionarMoradorController {
+public class adicionarMoradorController implements Initializable{
+       static ApartamentosDAO apartamentos = new ApartamentosDAO();
      @FXML
-    private ChoiceBox<?> Apartamento;
+    private ChoiceBox<Integer> Apartamento;
 
     @FXML
     private TextField Endereco;
@@ -42,6 +49,13 @@ public class adicionarMoradorController {
         String caminho = "/JavaFX/Pessoas/telaPessoa.fxml";
 
         controladorCena.voltarParaPaginaAnterior(event, caminho);
+    }
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ArrayList<Integer> dono = new ArrayList();
+        for(Apartamentos item: apartamentos.findAll()){
+         dono.add(item.getnumeroDoApartamento());
+        }
     }
 
 }
